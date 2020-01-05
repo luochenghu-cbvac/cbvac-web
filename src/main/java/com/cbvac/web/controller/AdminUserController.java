330e5bb9ddf41bdea57f6029217edbf841b166ce
luochenghu <luochenghu@cbvac.com>
2020-01-05 14:23:27 +0800

    udpate  get

--- src/main/java/com/cbvac/web/controller/AdminUserController.java
+++ src/main/java/com/cbvac/web/controller/AdminUserController.java
@@ -3,7 +3,10 @@ package com.cbvac.web.controller;
 import com.cbvac.web.service.AdminUserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author shaojieyue
@@ -21,7 +24,7 @@ public class AdminUserController {
         this.adminUserService = adminUserService;
     }
 
-    @PostMapping("login")
+    @GetMapping("login")
     public Object adminLogin(@RequestParam String name) {
         return adminUserService.findById(name);
     }
