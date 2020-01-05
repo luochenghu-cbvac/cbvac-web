ca5b7dd2e785877a07876d834f4b8a6753469676
luochenghu <luochenghu@cbvac.com>
2020-01-05 22:19:26 +0800

    常量类

---
+++ src/main/java/com/cbvac/common/Constant.java
@@ -0,0 +1,19 @@
+package com.cbvac.common;
+
+/**
+ * @author : Tiger
+ * @date : 2020-01-05 21:26
+ */
+public class Constant {
+
+    /**
+     * 成功
+     **/
+    public static final String httpStatusSuccess = "SUCCESS";
+
+    /**
+     * 失败
+     */
+    public static final String httpStatusFail = "FAIL";
+
+}
